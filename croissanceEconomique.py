4631cbc7b32bb8d4da2d0eae8e17fd3fd407e604
rovatohasina <rovatohasina22@gmail.com>
2025-04-06 20:29:56 +0200

    type

--- croissanceEconomique.py
+++ croissanceEconomique.py
@@ -91,6 +91,8 @@ min_year, max_year = df['Year'].min(), df['Year'].max()
 selected_years = st.sidebar.slider("Sélectionner une plage d'années", min_year, max_year, (min_year, max_year), 1)
 filtered_data = df[(df['Year'] >= selected_years[0]) & (df['Year'] <= selected_years[1])]
 filtered_data.dropna(inplace=True)
+st.write(df.dtypes)  # Vérifie le type des colonnes
+st.write(df['Year'].unique())
 # Titre
 st.title("Analyse de la croissance économique")
 st.write("")
